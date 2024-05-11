064d641d5bcad5835bb1d009ceb864a8c9998052
Tannmay Khandelwal <94522751+tannmaycoding@users.noreply.github.com>
2024-05-11 17:52:49 +0530

    Update ImageView.py

--- ImageView.py
+++ ImageView.py
@@ -1,15 +1,6 @@
 """
 This is an image viewing app using tkinter.
 In this app, you can view images in a given folder.
-
-Modules Used:
-    everything from tkinter,
-    Image, ImageTK from PIL,
-    os
-
-Functions made:
-    forward: for viewing the next image,
-    back: for viewing previous image
 """
 
 from tkinter import *
